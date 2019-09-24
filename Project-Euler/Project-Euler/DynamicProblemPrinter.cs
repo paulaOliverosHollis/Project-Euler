55f332b4ede4e75a967e0ac21090929467526c72
paulaOliverosHollis <paulahollis21@gmail.com>
2019-09-24 11:25:15 -0400

    Created initial classes and methods.
    Problems - this class will hold all of the problems as methods of their own.
    
    DynamicProblemPrinter - By using reflection, we can call any problem from the problems class which in turn will be shown to the screen.
    
    Program- I added some of the methods that will be use to interact with the user.

---
+++ Project-Euler/Project-Euler/DynamicProblemPrinter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler
+{
+    class DynamicProblemPrinter
+    {
+        public static void PrintProblem(int problemNumber, Type objectType)
+        {
+            var method = objectType.GetMethod($"Problem{problemNumber}");
+
+            method.Invoke(null, null);
+        }
+
+        public static void PrintAllProblems()
+        {
+            for (int i = 1; i < 26; i++)
+            {
+                PrintProblem(i, typeof(Problems));
+            }
+        }
+    }
+}
---
+++ Project-Euler/Project-Euler/Problems.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler
+{
+    public static class Problems
+    {
+        private static void Problem1()
+        {
+            Console.WriteLine("Problem: If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.");
+
+            int sum = 0;
+
+            for (int i = 3; i < 1000; i++)
+            {
+
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine($"Answer: {sum}");
+        }
+
+        private static void Problem2()
+        {
+            Console.WriteLine("Problem: By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.");
+        }
+    }
+}
