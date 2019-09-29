03c9707570b2362e46222c011706a72fe0c7ec5d
Paula Oliveros Hollis <paulaoliverosc@gmail.com>
2019-09-29 12:28:07 -0400

    Moved all helper methods into their own class.

---
+++ Project-Euler/Project-Euler/MathyHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler
+{
+    class MathyHelper
+    {
+        public static bool IsPrime(long number)
+        {
+            long squareRoot = (long)Math.Sqrt(number);
+
+            for (int i = 2; i <= squareRoot; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool IsPalendromic(string characters)
+        {
+            if (string.IsNullOrEmpty(characters))
+            {
+                return false;
+            }
+
+            // Comparing each character by starting from both ends of the string simultaneously.
+            for (int i = 0, j = characters.Length - 1; i < j; i++, j--)
+            {
+                if (characters[i] != characters[j])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool IsDivisibleFrom1To20(int number)
+        {
+            for (int i = 11; i < 21; i++)
+            {
+                if (number % i != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
