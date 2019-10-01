c0bae1c22077266c344daa0b5d2881972fdcbadc
Paula Oliveros Hollis <paulaoliverosc@gmail.com>
2019-10-01 09:31:30 -0400

    Removed TestCase from Problems_UnitTests.
    Removed some unwanted whitespaces.

--- Project-Euler/Project-Euler_UnitTests/Problems_UnitTests.cs
+++ Project-Euler/Project-Euler_UnitTests/Problems_UnitTests.cs
@@ -10,66 +10,59 @@ namespace Tests
     public class Problems_UnitTests
     {
         [Test]
-        [TestCase("233168")]
-        public void Problem1_ReturnsExpectedResult(string expectedResult)
+        public void Problem1_ReturnsExpectedResult()
         {
             string actualResult = Problems.Problem1();
 
-            Assert.AreEqual(expectedResult, actualResult);
+            Assert.AreEqual("233168", actualResult);
         }
 
         [Test]
-        [TestCase("4613732")]
-        public void Problem2_ReturnsExpectedResult(string expectedResult)
+        public void Problem2_ReturnsExpectedResult()
         {
             string actualResult = Problems.Problem2();
 
-            Assert.AreEqual(expectedResult, actualResult);
+            Assert.AreEqual("4613732", actualResult);
         }
 
         [Test]
-        [TestCase("6857")]
-        public void Problem3_ReturnsExpectedResult(string expectedResult)
+        public void Problem3_ReturnsExpectedResult()
         {
             string actualResult = Problems.Problem3();
 
-            Assert.AreEqual(expectedResult, actualResult);
+            Assert.AreEqual("6857", actualResult);
         }
 
         [Test]
-        [TestCase("906609")]
-        public void Problem4_ReturnsExpectedResult(string expectedResult)
+        public void Problem4_ReturnsExpectedResult()
         {
             string actualResult = Problems.Problem4();
 
-            Assert.AreEqual(expectedResult, actualResult);
+            Assert.AreEqual("906609", actualResult);
         }
 
         [Test]
-        [TestCase("232792560")]
-        public void Problem5_ReturnsExpectedResult(string expectedResult)
+        public void Problem5_ReturnsExpectedResult()
         {
             string actualResult = Problems.Problem5();
 
-            Assert.AreEqual(expectedResult, actualResult);
+            Assert.AreEqual("232792560", actualResult);
         }
 
         [Test]
-        [TestCase("25164150")]
-        public void Problem6_ReturnsExpectedResult(string expectedResult)
+        public void Problem6_ReturnsExpectedResult()
         {
             string actualResult = Problems.Problem6();
 
-            Assert.AreEqual(expectedResult, actualResult);
+            Assert.AreEqual("25164150", actualResult);
         }
 
         [Test]
-        [TestCase("104743")]
-        public void Problem7_ReturnsExpectedResult(string expectedResult)
+        public void Problem7_ReturnsExpectedResult()
         {
             string actualResult = Problems.Problem7();
 
-            Assert.AreEqual(expectedResult, actualResult);
+            Assert.AreEqual("104743", actualResult);
         }
     }
 }
