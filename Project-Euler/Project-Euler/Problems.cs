d88469f5253712de31a37930ea27c753283c6b28
Paula Oliveros Hollis <paulaoliverosc@gmail.com>
2019-11-03 12:38:53 -0500

    Started working on problem 8. The code compiles but does not give an accurate result yet.

--- Project-Euler/Project-Euler/Problems.cs
+++ Project-Euler/Project-Euler/Problems.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -163,6 +164,46 @@ namespace Project_Euler
             }
 
             return (number - 2).ToString();
+        }
+
+        public static string Problem8()
+        {
+            Console.WriteLine("Find the thirteenth adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?");
+
+            string bigInteger = "73167176531330624919225119674426574742355349194934" +
+                                "96983520312774506326239578318016984801869478851843" +
+                                "85861560789112949495459501737958331952853208805511" +
+                                "12540698747158523863050715693290963295227443043557" +
+                                "66896648950445244523161731856403098711121722383113" +
+                                "62229893423380308135336276614282806444486645238749" +
+                                "30358907296290491560440772390713810515859307960866" +
+                                "70172427121883998797908792274921901699720888093776" +
+                                "65727333001053367881220235421809751254540594752243" +
+                                "52584907711670556013604839586446706324415722155397" +
+                                "53697817977846174064955149290862569321978468622482" +
+                                "83972241375657056057490261407972968652414535100474" +
+                                "82166370484403199890008895243450658541227588666881" +
+                                "16427171479924442928230863465674813919123162824586" +
+                                "17866458359124566529476545682848912883142607690042" +
+                                "24219022671055626321111109370544217506941658960408" +
+                                "07198403850962455444362981230987879927244284909188" +
+                                "84580156166097919133875499200524063689912560717606" +
+                                "05886116467109405077541002256983155200055935729725" +
+                                "71636269561882670428252483600823257530420752963450";
+
+            BigInteger biggestProduct = 0;
+
+            for(int i = 0; i < bigInteger.Length - 13; i++)
+            {
+                BigInteger tempProduct = (int)bigInteger[i] * (int)bigInteger[i + 1] * (int)bigInteger[i + 2] * (int)bigInteger[i + 3] * (int)bigInteger[i + 4] * (int)bigInteger[i + 5] * (int)bigInteger[i + 6] * (int)bigInteger[i + 7] * (int)bigInteger[i + 8] * (int)bigInteger[i + 9] * (int)bigInteger[i + 10] * (int)bigInteger[i + 11] * (int)bigInteger[i + 12];
+
+                if(tempProduct > biggestProduct)
+                {
+                    biggestProduct = tempProduct;
+                }
+            }
+
+            return biggestProduct.ToString();
         }
     }
 }
